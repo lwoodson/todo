59604d17acc21e76bfd4c9d7e7fc7ccdb031d791
lwoodson <lance@webmaneuvers.com>
2013-06-08 15:48:05 -0500

    Todo::ItemRenderer and associated specs.

---
+++ lib/todo/item_renderer.rb
@@ -0,0 +1,49 @@
+module Todo
+  class ItemRenderer
+    FORMAT = "%-5s%s"
+    EMPTY_STR = ''
+    ITEM_SEPARATOR = "\n\n"
+    attr_reader :stream
+    def initialize(stream=STDOUT)
+      @stream = stream
+    end
+
+    def render_output(item_or_items)
+      items = Array(item_or_items)
+      items.each_with_index do |item, i|
+        stream.write(item_as_str(i, item))
+        stream.write(ITEM_SEPARATOR) unless is_last_item?(items, i)
+      end
+    end
+
+    private
+    def is_last_item?(items, index)
+      items.size == (index + 1)
+    end
+
+    def item_as_str(index, item)
+      "".tap do |result|
+        result << render_title(index, item)
+        item.lines.each do |line|
+          result << "\n#{render_line(line)}"
+        end
+      end
+    end
+
+    def render_title(index, item)
+      FORMAT % [index_str(index), item.title.upcase]
+    end
+
+    def render_line(line)
+      FORMAT % [EMPTY_STR, line]
+    end
+
+    def format(index, line)
+      FORMAT % [index, line]
+    end
+
+    def index_str(i)
+      "#{i + 1}."
+    end
+  end
+end
---
+++ spec/lib/todo/item_renderer_spec.rb
@@ -0,0 +1,132 @@
+require 'spec_helper'
+require 'stringio'
+
+describe Todo::ItemRenderer do
+  def rewind_after(&block)
+    block.call
+    @stream.rewind
+  end
+
+  before do
+    @stream = StringIO.new
+    @renderer = Todo::ItemRenderer.new(@stream)
+  end
+
+  describe "#render_output" do
+    context "when passed nil" do
+      it "should render nothing when passed a nil value" do
+        rewind_after {@renderer.render_output(nil)}
+        @stream.readlines.should == []
+      end
+    end
+
+    context "when passed single item" do
+      context "with a title only" do
+        before {@item = Todo::Item.new{|item| item.title = 'test'}}
+
+        it 'should render "1. [TITLE]" to the stream' do
+          rewind_after {@renderer.render_output(@item)}
+          @stream.read.should == "1.   TEST"
+        end
+      end
+
+      context "with a title and one line" do
+        before do
+          @item = Todo::Item.new do |item|
+            item.title = 'test'
+            item.append! 'This is a test.'
+          end
+        end
+
+        it 'should render "1. [TITLE]\n[LINE]" to the stream' do
+          rewind_after{@renderer.render_output(@item)}
+          @stream.read.should == "1.   TEST\n     This is a test."
+        end
+      end
+
+      context "with a title and two lines" do
+        before do
+          @item = Todo::Item.new do |item|
+            item.title = 'test'
+            item.append!('This is a test.')
+            item.append!('It is only a test.')
+          end
+        end
+
+        it 'should render "1. [TITLE]\n[LINE1]\n[LINE2]" to the stream' do
+          rewind_after {@renderer.render_output(@item)}
+          @stream.read.should == "1.   TEST\n     This is a test.\n     It is only a test."
+        end
+      end
+    end
+
+    context "when passed multiple items (title only)" do
+      context "with two items" do
+        before do
+          @items = [
+            Todo::Item.new {|item| item.title = 'one'},
+            Todo::Item.new {|item| item.title = 'two'}
+          ]
+        end
+
+        it 'should render "1. [TITLE]\n\n2.[TITLE]" to the stream' do
+          rewind_after {@renderer.render_output(@items)}
+          @stream.read.should == "1.   ONE\n\n2.   TWO"
+        end
+      end
+
+      context "with three items" do
+        before do
+          @items = [
+            Todo::Item.new {|item| item.title = 'one'},
+            Todo::Item.new {|item| item.title = 'two'},
+            Todo::Item.new {|item| item.title = 'three'}
+          ]
+        end
+
+        it 'should render "1.   [TITLE]\n\n2.  [TITLE]\n\n3.   [TITLE]" to the stream' do
+          rewind_after {@renderer.render_output(@items)}
+          @stream.read.should == "1.   ONE\n\n2.   TWO\n\n3.   THREE"
+        end
+      end
+    end
+
+    context "when passed a chipotle burrito worth of items" do
+      before do
+        @items = []
+        @items << Todo::Item.new do |item|
+          item.title = 'one'
+          item.append!('line 1.1')
+          item.append!('line 1.2')
+        end
+        @items << Todo::Item.new do |item|
+          item.title = 'two'
+          item.append!('line 2.1')
+          item.append!('line 2.2')
+        end
+        @items << Todo::Item.new do |item|
+          item.title = 'three'
+          item.append!('line 3.1')
+          item.append!('line 3.2')
+        end
+      end
+
+      it "should render a chipotle burrito worth of items" do
+        rewind_after {@renderer.render_output(@items)}
+        lines = @stream.readlines
+        lines.shift.should == "1.   ONE\n"
+        lines.shift.should == "     line 1.1\n"
+        lines.shift.should == "     line 1.2\n"
+        lines.shift.should == "\n"
+        lines.shift.should == "2.   TWO\n"
+        lines.shift.should == "     line 2.1\n"
+        lines.shift.should == "     line 2.2\n"
+        lines.shift.should == "\n"
+        lines.shift.should == "3.   THREE\n"
+        lines.shift.should == "     line 3.1\n"
+        lines.shift.should == "     line 3.2"
+        lines.shift.should == nil
+      end
+    end
+  end
+end
