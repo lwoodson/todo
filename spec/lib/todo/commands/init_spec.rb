417d0741dea0d7c2487e3d1db9e531cec64ba5f0
lwoodson <lance@webmaneuvers.com>
2013-06-09 20:28:06 -0500

    Test constants for dirs & files; Test DSL; modify init_spec to use DSL

--- spec/lib/todo/commands/init_spec.rb
+++ spec/lib/todo/commands/init_spec.rb
@@ -4,47 +4,39 @@ require 'fileutils'
 
 describe Todo::Commands::Init do
   before do
-    @parent = File.join(Dir.tmpdir, 'todo_tests')
-    @root_path = File.join(@parent, Todo::TODO_DIR)
-    @todo_path = File.join(@parent, Todo::TODO_FILE)
-    @working_path = File.join(@parent, Todo::WORKING_FILE)
-    @done_path = File.join(@parent, Todo::DONE_FILE)
-    @symlink_path = File.join(@parent, Todo::TODO_SYMLINK)
-    FileUtils.rm_rf(@parent)
-    FileUtils.mkdir_p(@parent)
-    @command = Todo::Commands::Init.new :parent => @parent
+    @command = Todo::Commands::Init.new :parent => test_parent
   end
 
   shared_examples_for "initialized project" do
     it "should have a .todo directory" do
       @command.execute
-      Dir.exists?(@root_path).should == true
+      Dir.exists?(todo_dir).should == true
     end
 
     it "should have a TODO file in the .todo directory" do
       @command.execute
-      File.exists?(@todo_path).should == true
+      File.exists?(todo_file).should == true
     end
 
     it "should have a WORKING file in the .todo directory" do
       @command.execute
-      File.exists?(@working_path).should == true
+      File.exists?(working_file).should == true
     end
 
     it "should have a DONE file in the .todo directory" do
       @command.execute
-      File.exists?(@done_path).should == true
+      File.exists?(done_file).should == true
     end
 
     it "should have a symlink from $ROOT/TODO to $ROOT/.todo/TODO" do
       @command.execute
-      File.symlink?(@symlink_path).should == true
+      File.symlink?(todo_symlink).should == true
     end
   end
 
   shared_examples_for "TODO preserver" do
     it "should preserve original contents of TODO" do
-      File.read(@symlink_path).should == '* test'
+      todo_contents.should == '* test'
     end
   end
 
@@ -57,7 +49,7 @@ describe Todo::Commands::Init do
   context "encountering an existing TODO" do
     describe "#execute" do
       before do
-        File.write(@symlink_path, '* test')
+        write_todo('* test')
       end
 
       it_behaves_like "initialized project"
@@ -68,9 +60,9 @@ describe Todo::Commands::Init do
   context "encountering an existing todo project" do
     describe "#execute" do
       before do
-        FileUtils.mkdir_p(@root_path)
-        File.write(@todo_path, '* test')
-        FileUtils.ln_s(@todo_path, @symlink_path)
+        create_todo_dir
+        create_symlink
+        write_todo( '* test')
       end
 
       it_behaves_like "initialized project"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,64 @@
 $: << 'lib'
 
 require 'todo'
+require 'tmpdir'
+
+module Todo
+  module Tests
+    PARENT = File.join(Dir.tmpdir, 'todo_tests')
+    TODO_DIR = File.join(PARENT, Todo::TODO_DIR)
+    TODO_FILE = File.join(PARENT, Todo::TODO_FILE)
+    WORKING_FILE = File.join(PARENT, Todo::WORKING_FILE)
+    DONE_FILE = File.join(PARENT, Todo::DONE_FILE)
+    TODO_SYMLINK = File.join(PARENT, Todo::TODO_SYMLINK)
+
+    def test_parent
+      PARENT
+    end
+
+    def todo_dir
+      TODO_DIR
+    end
+
+    def todo_file
+      TODO_FILE
+    end
+
+    def working_file
+      WORKING_FILE
+    end
+
+    def done_file
+      DONE_FILE
+    end
+
+    def todo_symlink
+      TODO_SYMLINK
+    end
+
+    def create_todo_dir
+      FileUtils.mkdir_p(TODO_DIR)
+    end
+
+    def create_symlink
+      FileUtils.ln_s(TODO_FILE, TODO_SYMLINK)
+    end
+
+    def write_todo(text)
+      File.write(TODO_SYMLINK, text)
+    end
+
+    def todo_contents
+      File.read(Todo::Tests::TODO_SYMLINK)
+    end
+  end
+end
+
+
+RSpec.configure do |config|
+  config.before(:each) do
+    self.extend Todo::Tests
+    FileUtils.rm_rf(Todo::Tests::PARENT)
+    FileUtils.mkdir_p(Todo::Tests::PARENT)
+  end
+end
