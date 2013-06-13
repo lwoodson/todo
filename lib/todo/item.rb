b0ceab927686c24c5c977b2c5a265562310da960
lwoodson <lance@webmaneuvers.com>
2013-06-13 05:43:13 -0500

    Added status methods to set & test the status of Todo::Items

--- lib/todo/item.rb
+++ lib/todo/item.rb
@@ -2,13 +2,17 @@ require 'fileutils'
 
 module Todo
   class Item
-    attr_accessor :index, :title, :status, :worker, :description
-    attr_reader :children
+    TODO_STATUS = 'todo'
+    WORKING_STATUS = 'working'
+    DONE_STATUS = 'done'
+    STATUSES = [TODO_STATUS, WORKING_STATUS, DONE_STATUS]
+    attr_accessor :index, :title, :worker, :description
+    attr_reader :children, :status
 
     def initialize(opts={})
       @index = opts[:index]
       @title = opts[:title]
-      @status = opts[:status]
+      @status = opts[:status] || TODO_STATUS
       @worker = opts[:worker]
       @description = opts[:description]
       @children = opts[:children] || []
@@ -24,9 +28,20 @@ module Todo
       children == other.children
     end
 
+    STATUSES.each do |status|
+      define_method("#{status}!") do
+        @status = status
+      end
+
+      define_method("#{status}?") do
+        @status == status
+      end
+    end
+
     private
     def validate!
       raise "Index must be positive integer" if index.to_i < 1
+      raise "Status must be #{STATUSES.join(', ')}" unless STATUSES.include?(status)
     end
   end
 end
--- spec/lib/todo/item_spec.rb
+++ spec/lib/todo/item_spec.rb
@@ -19,13 +19,49 @@ describe Todo::Item do
     it "should not raise an error if the index is a positive integer" do
       expect{Todo::Item.new :index => 1}.to_not raise_error
     end
+
+    it "should raise an error if the status is not one of todo, working or done" do
+      expect{Todo::Item.new :index => 1, :status => 2}.to raise_error
+    end
+
+    Todo::Item::STATUSES.each do |status|
+      it "should not raise error if the status is #{status}" do
+        expect{Todo::Item.new :index => 1, :status => status}.to_not raise_error
+      end
+    end
+  end
+
+  context "status related methods" do
+    before {@item = Todo::Item.new :index => 1}
+    Todo::Item::STATUSES.each do |status|
+      describe "##{status}!" do
+        it "should put the item in a #{status} status" do
+          @item.send("#{status}!")
+          @item.status.should == status
+        end
+      end
+
+      describe "##{status}?" do
+        it "should be true if the item is in a #{status} status" do
+          @item.send("#{status}!")
+          @item.send("#{status}?").should == true
+        end
+
+        (Todo::Item::STATUSES - [status]).each do |other_status|
+          it "should be false if the item is in a #{other_status} status" do
+            @item.send("#{other_status}!")
+            @item.send("#{status}?").should == false
+          end
+        end
+      end
+    end
   end
 
   describe "#==" do
      before do 
-      @item1 = Todo::Item.new :index => 1, :title => 'title', :status => 'status', 
+      @item1 = Todo::Item.new :index => 1, :title => 'title', :status => Todo::Item::TODO_STATUS,
                      :worker => 'worker', :description => 'desc', :children => [1]
-      @item2 = Todo::Item.new :index => 1, :title => 'title', :status => 'status', 
+      @item2 = Todo::Item.new :index => 1, :title => 'title', :status => Todo::Item::TODO_STATUS,
                      :worker => 'worker', :description => 'desc', :children => [1]
     end
 
@@ -33,7 +69,7 @@ describe Todo::Item do
       @item1.should == @item2
     end
 
-    %w[index title status worker description].each do |attr|
+    %w[index title worker description].each do |attr|
       it "should treat items with different #{attr}s as not equal" do
         @item2.send("#{attr}=", "changed")
         @item1.should_not == @item2
